c4d7a548b9991543606b152ad0efa3fdbc782575
ongvang00 <you@example.com>
2023-04-25 21:00:41 -0400

    asfasf

--- script.js
+++ script.js
@@ -1,47 +1,41 @@
+
+$(document).ready(function() {
 //Create a function that will fire when the submit <button> is clicked
-const submitBtn = $('#submit').on("click", function(){
-    console.log ("submitBtn is clicked")
+    $('#submit').on('click', function(e) {
+        e.preventDefault();
 //create variables for each of the form inputs
-    const nameInput = $('#name').val();
-    const emailInput = $('#email').val();
-    const phoneInput = $('#phone').val();
-    const labelTag = $("label");
-    const labelWarningClass = $(".warning")
- 
+        let name = $("#name");
+        let email = $("#email");
+        let phone = $("#phone");
 //create an array named required
-    const required = [nameInput, emailInput, phoneInput];
-    
-//Utilize a for loop to iterate of the entire required array   
-    for(let i=0; i<3; i++){ 
-//if the array item is equal to an empty string ("") populate the message <p> with the following text:
-      let elmLabel = required[i].prev();
-    if (required[i] === ""){
-            $("#message").html("Please fill out the required fields");
+        let required = [name, email, phone];
+//Utilize a for loop to iterate of the entire required array
+        for(let item of required) {
+//use an if statement to check the value of each array item
+//if the array item is equal to an empty string ("")
+            if (item.val() == "") {
+//populate the message <p> with the following text:
+//"Please Fill Out Required Fields
+                $("#message").text("Please Fill Out Required Fields");
 //add the warning class to this message
-////Attach the warning class to the array item's <label>
-//note: You want the label to turn red, if the field is empty
-        
-            $("#message").attr("class","warning");
-            $("label").addClass("warning");
-            required[i].prev().attr("class", "warning");
-        } else {
+                $("#message").addClass("warning");
+//Attach the warning class to the array item's <label>
+                item.prev('label').addClass('warning');
+            }
 //once the field has something other than a blank string. make sure the warning class is removed
-            $("#message").html("");
-            elmLabel.removeClass("warning");
-
-        }
+            else {
+                item.prev('label').removeClass('warning'); 
+            }
 //close out the for loop
-}
-
-    if (!($("label").hasClass("warning"))){
-  
+        }
+//utilize another if statement
+//verify that no <label>'s have the warning class
+        if(!$('label').hasClass('warning')) {
 //remove the form from the DOM
-    $('#form').remove();
-    
+            $('#form').remove(); 
 //manipulate the <h2> to say: "Thanks for your feedback!"
-    $("h2").html("Thanks for the feedback");
-    
-    return true 
-}
-}
-)
+            $('#pre-form > h2').text('Thanks for your feedback!');
+//close your function
+        }
+    });
+});
