c54c3669059f42cdf65a0e8d2f1a5f1dbfe38c9f
ongvang00 <you@example.com>
2023-04-25 20:52:26 -0400

    asf

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 //Create a function that will fire when the submit <button> is clicked
 const submitBtn = $('#submit').on("click", function(){
     console.log ("submitBtn is clicked")
@@ -13,35 +12,36 @@ const submitBtn = $('#submit').on("click", function(){
     const required = [nameInput, emailInput, phoneInput];
     
 //Utilize a for loop to iterate of the entire required array   
-    for(let i=0; i<1; i++){ 
+    for(let i=0; i<3; i++){ 
 //if the array item is equal to an empty string ("") populate the message <p> with the following text:
-        if (required[i] == ""){
-            $("#submit").before(`<p>"Please fill out the required fields"</p>`);
+      let elmLabel = required[i].prev();
+    if (required[i] === ""){
+            $("#message").html("Please fill out the required fields");
 //add the warning class to this message
 ////Attach the warning class to the array item's <label>
 //note: You want the label to turn red, if the field is empty
         
-            $("p").addClass("warning");
+            $("#message").attr("class","warning");
             $("label").addClass("warning");
+            required[i].prev().attr("class", "warning");
         } else {
 //once the field has something other than a blank string. make sure the warning class is removed
-            $("label").removeClass("warning")
+            $("#message").html("");
+            elmLabel.removeClass("warning");
 
-}
+        }
 //close out the for loop
 }
-    if (labelTag !== labelWarningClass){
+
+    if (!($("label").hasClass("warning"))){
   
 //remove the form from the DOM
- 
-$('#form').remove('#form');
+    $('#form').remove();
     
 //manipulate the <h2> to say: "Thanks for your feedback!"
-
-$("h2:contains('We love to hear your thoughts!')").replaceWith("Thanks for your feedback!");
-return true 
+    $("h2").html("Thanks for the feedback");
+    
+    return true 
 }
 }
 )
-
-
